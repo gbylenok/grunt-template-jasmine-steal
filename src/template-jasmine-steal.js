b9ba9fd424053e09a544d2b0eb2596c24b91a36e
Greg B <greg.bylenok@cengage.com>
2014-11-07 12:29:30 -0500

    re-add temp directory

--- src/template-jasmine-steal.js
+++ src/template-jasmine-steal.js
@@ -2,6 +2,9 @@
 "use strict";
 
 var template = __dirname + '/templates/jasmine-steal.html';
+//    steal  = {
+//      '3.2.3' : __dirname + '/../vendor/steal-3.2.3'
+//    };
 
 exports.process = function(grunt, task, context) {
   context.options.stealOptions = context.options.stealOptions || {};
@@ -10,7 +13,7 @@ exports.process = function(grunt, task, context) {
 
   // find the latest version if none given
   if (!stealUrl) {
-    stealUrl = _dirname + '/steal/steal.js';
+    stealUrl = '.grunt/grunt-contrib-jasmine/steal/steal.js';
   }
 
   var stealRoot = stealUrl.substring(0, stealUrl.indexOf('steal/steal.js'));
@@ -24,15 +27,20 @@ exports.process = function(grunt, task, context) {
           supportRel = /^\.grunt\//,
           absBase = '';
 
-      if(s.indexOf(stealRoot) === -1 && s.indexOf('node_modules') === -1) {
+      if(s.indexOf(stealRoot) === -1 && s.indexOf('.grunt') === -1) {
         absBase = '/';
       }
 
-      var result = absBase + s.replace(stealRel, '').replace(baseRel, '').replace(supportRel, '/node_modules/');
-      console.log('pathify: '+s+' to '+result+ ' absBase: '+absBase+ ' stealRel: '+stealRel+' baseRel: '+baseRel+' stealRoot'+stealRoot);
-      return result;
+        var result = absBase + s.replace(stealRel, '').replace(baseRel, '').replace(supportRel, '/node_modules/');
+        console.log('pathify: '+s+' to '+result+ ' absBase: '+absBase+ ' stealRel: '+stealRel+' baseRel: '+baseRel+' stealRoot'+stealRoot);
+        //grunt.verbose.write
+        return result;
     }
   };
+
+  // put steal into place
+//  task.copyTempFile(steal['3.2.3'] + '/steal.js','steal/steal.js');
+//  task.copyTempFile(steal['3.2.3'] + '/dev/dev.js','steal/dev/dev.js');
 
   var source = grunt.file.read(template);
   return grunt.util._.template(source, context);
